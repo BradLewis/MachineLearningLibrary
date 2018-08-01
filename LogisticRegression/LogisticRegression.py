502dce51cd2be3c119b8010f26e040f0fa19e120
Bradley Lewis <blewis2109@gmail.com>
2018-08-01 16:41:38 -0400

    Finished example for logistic regression

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def logistic_regression(features, preditor, steps, learning_rate, add_intercept = False):
+def logistic_regression(features, predictor, steps, learning_rate, add_intercept):
     if add_intercept:
         intercept = np.ones((features.shape[0], 1))
         features = np.hstack((intercept, features))
@@ -11,21 +11,23 @@ def logistic_regression(features, preditor, steps, learning_rate, add_intercept 
         scores = np.dot(features, weights)
         predictions = logistic_function(scores)
 
-        output_error_signal = preditor - predictions
+        output_error_signal = predictor - predictions
         gradient = np.dot(features.T, output_error_signal) 
         weights += learning_rate * gradient
+
+        if step % 10000 == 0:
+            print(log_likelihood(features, weights, predictor))
 
     return weights
 
 def logistic_function(scores):
     # Returns the sigmoid funciton for the given value
-    return 1/ (1 + np.exp(-scores))
+    return 1 / (1 + np.exp(-scores))
 
 def log_likelihood(features, weights, predictor):
     # Calculates the log-likelihood
     scores = np.dot(features, weights)
-    normalization_factor = np.exp(scores) + np.exp(-scores)
-    ll = (1 / normalization_factor) * np.sum(np.exp(predictor * scores))
+    ll = np.sum(predictor*scores - np.log(1 + np.exp(scores)))
     return ll
 
 if __name__ == '__main__':
