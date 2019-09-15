f3a411c1a8f85491e68b7953d6bdca6a83a0fef8
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2019-09-15 12:29:49 -0400

    Adding the derivatives

--- machineLearningLibrary/activationFunctions.py
+++ machineLearningLibrary/activationFunctions.py
@@ -14,15 +14,29 @@ class Sigmoid(ActivationFunction):
     def get(self, z):
         return 1 / (1 + np.exp(-z))
 
+    def getDerivative(self, z):
+        a = self.get(z)
+        return a * (1 - a)
+
 
 class Tanh(ActivationFunction):
     def get(self, z):
         return np.tanh(z)
 
+    def getDerivative(self, z):
+        a = self.get(z)
+        return 1 - np.power(a, 2)
+
 
 class Relu(ActivationFunction):
     def get(self, z):
         return max(0, z)
+
+    def getDerivative(self, z):
+        if z < 0:
+            return 0
+        else:
+            return 1
 
 
 class LeakyRelu(ActivationFunction):
@@ -31,3 +45,9 @@ class LeakyRelu(ActivationFunction):
 
     def get(self, z):
         return max(self.a * z, z)
+
+    def getDerivative(self, z):
+        if z < 0:
+            return self.a
+        else:
+            return 1
