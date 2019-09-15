216c25ac816fa93f66c5061c006f320af3e52024
Brad Lewis <22850972+BradLewis@users.noreply.github.com>
2019-09-15 12:34:56 -0400

    Updating unit tests for activation functions

--- tests/test_activationFucntions.py
+++ tests/test_activationFucntions.py
@@ -1,5 +1,26 @@
-from machineLearningLibrary.activationFunctions import sigmoid
+from machineLearningLibrary.activationFunctions import (Sigmoid,
+                                                        Tanh,
+                                                        Relu,
+                                                        LeakyRelu)
 
 
 def test_sigmoid():
-    assert sigmoid(0) == 0.5
+    sigmoid = Sigmoid()
+    assert sigmoid.get(0) == 0.5
+
+
+def test_tanh():
+    tanh = Tanh()
+    assert tanh.get(0) == 0
+
+
+def test_relu():
+    relu = Relu()
+    assert relu.get(5) == 5
+    assert relu.get(-5) == 0
+
+
+def test_leakyRelu():
+    leakyRelu = LeakyRelu(0.1)
+    assert leakyRelu.get(5) == 5
+    assert leakyRelu.get(-5) == -0.5
